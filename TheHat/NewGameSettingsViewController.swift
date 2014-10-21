7a0c8825a33d5fe3d1363810ca211cf5a1580baf
Vadim Drobinin <vadim@drobinin.com>
2014-10-21 13:03:03 +0400

    Added modal transitions between Views.

---
+++ TheHat/NewGameSettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NewGameSettingsViewController.swift
+//  TheHat
+//
+//  Created by Vadim Drobinin on 21/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class NewGameSettingsViewController: UIViewController {
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonAction(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
