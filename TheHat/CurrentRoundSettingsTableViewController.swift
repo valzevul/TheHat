306d5c21503d49af3483038bdd531c871f1b1723
Vadim Drobinin <vadim@drobinin.com>
2014-10-26 21:29:09 +0300

    Added Settings screen for current round.

---
+++ TheHat/CurrentRoundSettingsTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentRoundSettingsTableViewController.swift
+//  TheHat
+//
+//  Created by Vadim Drobinin on 26/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRoundSettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
