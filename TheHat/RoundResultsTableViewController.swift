a64bfe364db369d21ec7a385968d65d0c6377c6c
Vadim Drobinin <vadim@drobinin.com>
2014-10-28 00:13:42 +0300

    Expandable cells rewritten for delegates.

--- TheHat/RoundResultsTableViewController.swift
+++ TheHat/RoundResultsTableViewController.swift
@@ -23,7 +23,6 @@ class RoundResultsTableViewController: UITableViewController, UITableViewDataSou
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
         self.cells.append(ActiveWord(word: currentWord!, status: "?"))
         self.navigationItem.setHidesBackButton(true, animated: true)
     }
@@ -34,60 +33,38 @@ class RoundResultsTableViewController: UITableViewController, UITableViewDataSou
     }
     
     
-    // TODO:
-    // Implement this one:
-    // http://www.appcoda.com/swipeable-uitableviewcell-tutorial/
-    
-    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.cells.count
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        switch (indexPath.row) {
+    func swipeTableCell(cell: MGSwipeTableCell!, tappedButtonAtIndex index: Int, direction: MGSwipeDirection, fromExpansion: Bool) -> Bool {
+        switch (index) {
+        case 0:
+            println("OK")
         case 1:
-            println("guessed")
-            tSystem!.wordGuessed()
+            println("F")
         case 2:
-            println("failed")
-            tSystem!.wordFailed()
-        case 3:
-            println("missed")
-            tSystem!.wordMissed(currentWord!)
+            println("?")
         default:
-            println("other row")
+            println("x")
         }
-        
+        return true
     }
-    
-//    func swipeTableCell(cell: MGSwipeTableCell!, tappedButtonAtIndex index: Int, direction: MGSwipeDirection, fromExpansion: Bool) -> Bool {
-//        println("lol")
-//        return false
-//    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: CustomTableViewCell = tableView.dequeueReusableCellWithIdentifier("CustomCell") as CustomTableViewCell
         let word = self.cells[indexPath.row]
         let image = UIImage(named: word.getStatus());
         
+        cell.delegate = self
+        
         cell.wordLabel?.text = word.getText()
         cell.wordResultImage.image = image
         cell.leftSwipeSettings.transition = MGSwipeTransition.Transition3D
         cell.leftButtons = [
-            MGSwipeButton(title: "OK", backgroundColor: UIColor(red: 0.07, green: 0.75, blue: 0.16, alpha: 0.7), callback: { (cell) -> Bool in
-                println("OK")
-                return true
-            }),
-            MGSwipeButton(title: "F", backgroundColor: UIColor(red: 1.0, green: 0.231, blue: 0.188, alpha: 0.7), callback: { (cell) -> Bool in
-                println("F")
-                return true
-            }),
-            MGSwipeButton(title: "?", backgroundColor: UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 0.7), callback: { (cell) -> Bool in
-                println("?")
-                return true
-            })
-                            ]
+            MGSwipeButton(title: "OK", backgroundColor: UIColor(red: 0.07, green: 0.75, blue: 0.16, alpha: 0.7)),
+            MGSwipeButton(title: "F", backgroundColor: UIColor(red: 1.0, green: 0.231, blue: 0.188, alpha: 0.7)),
+            MGSwipeButton(title: "?", backgroundColor: UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 0.7))]
         return cell
     }
     
