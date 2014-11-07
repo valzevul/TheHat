062badffc6df06dd4f5437bb5a60d667d1ab98b4
Vadim Drobinin <vadim@drobinin.com>
2014-11-07 14:51:49 +0300

    Some refactoring for the first app screen.

--- TheHat/StartScreenViewController.swift
+++ TheHat/StartScreenViewController.swift
@@ -10,48 +10,54 @@ import UIKit
 
 class StartScreenViewController: UIViewController {
 
-    
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var multiplayerGameButton: UIButton!
     @IBOutlet weak var settingsBarButton: UIBarButtonItem!
     @IBOutlet weak var infoBarButton: UIBarButtonItem!
 
-    var mask: CALayer!
-    var firstRun = true
+    var mask: CALayer! // Animation mask
+    var firstRun = true // Flag for animation
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if (firstRun) {
-            self.mask = CALayer()
-            self.mask!.contents = UIImage(named: "thehat").CGImage
-            self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
-            self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-            self.mask!.position = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
-            self.view.layer.mask = mask
-            
-            let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
-            keyFrameAnimation.duration = 1
-            keyFrameAnimation.delegate = self
-            keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-            let initalBounds = NSValue(CGRect: mask!.bounds)
-            let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 90))
-            let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1500, height: 1500))
-            keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
-            keyFrameAnimation.keyTimes = [0, 0.3, 1]
-            self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+        if (firstRun) { // Shows animated hat screen
+            createLayerMask()
+            setKeyFrameAnimation()
             firstRun = false
         }
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func createLayerMask() {
+        self.mask = CALayer()
+        self.mask!.contents = UIImage(named: "thehat").CGImage
+        self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask!.position = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
+        self.view.layer.mask = mask
+    }
+    
+    func setKeyFrameAnimation() {
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.duration = 1
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
+        
+        setBounds(keyFrameAnimation)
+    }
+    
+    func setBounds(keyFrameAnimation: CAKeyframeAnimation) {
+        let initalBounds = NSValue(CGRect: mask!.bounds)
+        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 90))
+        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1500, height: 1500))
+        
+        keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
     }
 
     override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
-        self.view.layer.mask = nil //remove mask when animation completes
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.layer.mask = nil // Remove mask when animation completes
     }
 
     
@@ -63,9 +69,6 @@ class StartScreenViewController: UIViewController {
     }
     @IBAction func settingsBarButtonAction(sender: UIBarButtonItem) {
     }
-
-    
-    
 
 }
 
