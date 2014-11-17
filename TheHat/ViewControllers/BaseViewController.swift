a79e884243bd7dbaa6e779da85831561a06c5220
Vadim Drobinin <vadim@drobinin.com>
2014-11-17 16:56:52 +0300

    Added global background color

--- TheHat/ViewControllers/BaseViewController.swift
+++ TheHat/ViewControllers/BaseViewController.swift
@@ -13,23 +13,8 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    self.view.backgroundColor = UIColor(red: 175.0/255.0, green: 240.0/255.0, blue: 203.0/255.0, alpha: 1.0) // Global background colour
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
