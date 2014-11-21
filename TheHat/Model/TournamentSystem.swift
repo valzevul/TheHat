b485a382cc1f6294f6187289aa6639f0a51e1eab
Vadim Drobinin <vadim@drobinin.com>
2014-11-21 14:52:42 +0300

    Refactored Tournament System.

--- TheHat/Model/TournamentSystem.swift
+++ TheHat/Model/TournamentSystem.swift
@@ -8,27 +8,56 @@
 
 import Foundation
 
+/// Tournament Systems maintains rounds and operations for interaction with them.
 class TournamentSystem {
+    
+    /// Game object with players and Active Words
     let gameObject: Game
     
+    /// First player in pair
     var playerIdx = 0
+    
+    /// Number of played rounds
     var playedRoundsNumber: Int = 0
     
-    var currentActiveWords = [ActiveWord]() // Current round played (!) words
+    /// Words played in the current round
+    var currentActiveWords = [ActiveWord]()
+    
+    /// Results of the current round
     var currentResult: Int = 0
+    
+    /// Current pair of Players
     var currentPair: (Player, Player)?
     
+    
+    /**
+    Initializes a new Tournament System object.
+    
+    :param: game Game object
+    :returns: new Tournament System object with a link to the Game object.
+    
+    */
     init(game: Game) {
         gameObject = game
     }
     
     // MARK: - Words
     
+    /**
+    Returns number of words left in the game.
+
+    :returns: Int number of words left in the game.
+    */
     func wordsLeft() -> Int {
         return gameObject.wordsLeft
     }
     
-    func getNewWord() -> ActiveWord? { // Returns new word and remove it from the list
+    /**
+    Returns a new Active word and removes it from the list of unguessed words.
+
+    :returns: Active word or null if there is no more.
+    */
+    func getNewWord() -> ActiveWord? {
         if (gameObject.words.count > 0) {
             return gameObject.words.removeLast()
         } else {
@@ -36,41 +65,56 @@ class TournamentSystem {
         }
     }
     
-    func shuffleWords() {
-        //
-    }
+    /**
+    Apply new results to statistics and update the list of active words with a new guessed now.
     
+    :param: word Active Word which was guessed
+    */
     func wordGuessed(word: ActiveWord) {
-        currentResult += 1
+        currentResult += 1 // Number of words guessed by the pair
         (currentPair!.0).incScoreExplained()
         (currentPair!.1).incScoreGuessed()
         word.changeStatus("OK")
         currentActiveWords.append(word)
     }
     
+    /**
+    Apply new results to statistics and update the list of active words with a new failed now.
+    
+    :param: word Active Word which was failed
+    */
     func wordFailed(word: ActiveWord) {
         word.changeStatus("F")
         currentActiveWords.append(word)
-        finishCurrentRound()
     }
     
+    /**
+    Apply new results to statistics and update the list of active words with a new missed now.
+    
+    :param: word Active Word which was missed
+    */
     func wordMissed(word: ActiveWord) {
-        if (word.getStatus() == "?") {
-            
-            gameObject.words.insert(word, atIndex: 0)
+        if (word.getStatus() == "?") { // If word is really missed, not failed
+            gameObject.words.insert(word, atIndex: 0) // Returns it back to the list of words
             currentActiveWords.append(word)
         }
     }
     
     // MARK: - Players
     
+    
+    /**
+    Generate a new pair of players for the game with a step of two.
+    
+    :returns: (Player, Player) tuple or nil if there is no new games
+    */
     func getNextPair() -> (Player, Player)? { // Create new pair of players
         var player_1 = gameObject.getPlayerByIndex(playerIdx)
         var player_2 = gameObject.getPlayerByIndex(playerIdx + 1)
         
         playerIdx += 2
         if (playerIdx + 1 > gameObject.players.count) {
-            playerIdx = 0
+            playerIdx = 0 // Starts from the first player for the next "round" of rounds
         }
         
         currentPair = (player_1, player_2)
@@ -79,23 +123,29 @@ class TournamentSystem {
     
     // MARK: - Round
     
-    func startNextRound() { // Imitate new game round
+    /**
+    Starts new round.
+    
+    */
+    func startNextRound() {
         currentActiveWords = [ActiveWord]()
         playedRoundsNumber += 1
-        var currentPair = getNextPair()
-        shuffleWords()
+        var currentPair = getNextPair() // Or without "var", it depends // TODO: Change this structure
     }
     
-    func finishCurrentRound() {
-        // ???
-    }
+    /**
+    Returns a result of the previous round.
     
+    :returns: Int result or nil if this is the first round
+    */
     func getPreviousResult() -> Int? {
         return currentResult
     }
     
+    /**
+    Starts new round with wiping all previous results.
+    */
     func clean() {
         currentResult = 0
-        shuffleWords()
     }
 }
