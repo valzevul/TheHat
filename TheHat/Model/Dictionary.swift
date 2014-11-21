2d1faec2e176995fc455691b57ed39451ba7a9dc
Vadim Drobinin <vadim@drobinin.com>
2014-11-21 15:34:56 +0300

    Refactored Dictionary object.

--- TheHat/Model/Dictionary.swift
+++ TheHat/Model/Dictionary.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
-extension Array { // Shuffle sort implementation
+
+/// Extension for the Array class to implement the random shuffle sort.
+extension Array {
     mutating func shuffle() {
         for _ in 0..<10 {
             sort {
@@ -18,29 +20,54 @@ extension Array { // Shuffle sort implementation
     }
 }
 
+
+/// Dictionary class to operate with parsed from a file words
 class Dictionary {
     
+    /// File to be parsed
     let filename: String
+    
+    /// List of words from the file
     var textWords = [String]() // Words from file
     
+    /// Number of processed words
     var wordsReturned = 0
     
+    /**
+    Initializes Dictionary object with name of the file to be parsed.
+    
+    :param: filename String name of the file
+    */
     init(filename: String) {
         self.filename = filename
     }
     
+    /**
+    Get new word from the list.
+    
+    :return: String word from file
+    */
     func getNewWord() -> String? { // Returns new word
         wordsReturned += 1
         return getNewWordByIndex(wordsReturned - 1)
     }
     
-    func getNewWordByIndex(index: Int) -> String? { // Returns new word by index
+    /**
+    Returns new word by index (i.e for the random shuffle)
+    
+    :param: index Int index of the word
+    :return: String word or nil if the index is out of bound
+    */
+    func getNewWordByIndex(index: Int) -> String? {
         if (index < 0 || index >= textWords.count) {
             return nil
         }
         return textWords[index]
     }
     
+    /**
+    Parses words from a file and shuffle them in the list.
+    */
     func parse() {
         
         let bundle = NSBundle.mainBundle()
