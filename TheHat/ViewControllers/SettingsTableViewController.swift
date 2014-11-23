6cb0b0fa8c24aec15668c35896926cd0a2270e05
Vadim Drobinin <vadim@drobinin.com>
2014-11-23 18:11:14 +0300

    Settings changed to steppers instead of fields.

--- TheHat/ViewControllers/SettingsTableViewController.swift
+++ TheHat/ViewControllers/SettingsTableViewController.swift
@@ -14,20 +14,23 @@ class SettingsTableViewController: UITableViewController, UITextFieldDelegate {
     
     @IBOutlet weak var showTutorialSwitch: UISwitch!
     @IBOutlet weak var playersNameFIeld: UITextField!
-    @IBOutlet weak var playersNumberField: UITextField!
     @IBOutlet weak var difficultnessSlider: UISlider!
-    @IBOutlet weak var gameTimeField: UITextField!
-    @IBOutlet weak var additionalTimeField: UITextField!
-    @IBOutlet weak var wordsPerPlayerField: UITextField!
+    
+    @IBOutlet weak var playersNumberStepper: UIStepper!
+    @IBOutlet weak var wordsPerPlayerStepper: UIStepper!
+    @IBOutlet weak var gameTimeStepper: UIStepper!
+    @IBOutlet weak var additionalTimeStepper: UIStepper!
+    
+    @IBOutlet weak var playersNumberLabel: UILabel!
+    @IBOutlet weak var playersWordsLabel: UILabel!
+    @IBOutlet weak var gameTimeLabel: UILabel!
+    @IBOutlet weak var additionalTimeLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         playersNameFIeld.delegate = self
-        playersNumberField.delegate = self
-        gameTimeField.delegate = self
-        additionalTimeField.delegate = self
-        wordsPerPlayerField.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -53,23 +56,52 @@ class SettingsTableViewController: UITableViewController, UITextFieldDelegate {
             playersNameFIeld.text = pName
         }
         if let pNumber = namePreference.stringForKey("playersNumber") {
-            playersNumberField.text = pNumber
+            playersNumberStepper.value = (pNumber as NSString).doubleValue
         }
         if let diff = namePreference.stringForKey("difficultness") {
             difficultnessSlider.value = (diff as NSString).floatValue
         }
         if let gTime = namePreference.stringForKey("gameTime") {
-            gameTimeField.text = gTime
+            gameTimeStepper.value = (gTime as NSString).doubleValue
         }
         if let aTime = namePreference.stringForKey("additionalTime") {
-            additionalTimeField.text = aTime
+            additionalTimeStepper.value = (aTime  as NSString).doubleValue
         }
         if let pWords = namePreference.stringForKey("playerWords") {
-            wordsPerPlayerField.text = pWords
+            wordsPerPlayerStepper.value = (pWords as NSString).doubleValue
         }
     }
     
     // MARK: - Settings Saving
+
+    @IBAction func PlayersNumberValueChanged(sender: UIStepper) {
+        playersNumberLabel.text = "\(sender.value)"
+        
+        NSUserDefaults.standardUserDefaults().setValue(sender.value, forKey:Settings.keys[sender.tag])
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    @IBAction func WordsNumberValueChanged(sender: UIStepper) {
+        playersWordsLabel.text = "\(sender.value)"
+        
+        NSUserDefaults.standardUserDefaults().setValue(sender.value, forKey:Settings.keys[sender.tag])
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    @IBAction func GameTimeValueChanged(sender: UIStepper) {
+        gameTimeLabel.text = "\(sender.value)"
+        
+        NSUserDefaults.standardUserDefaults().setValue(sender.value, forKey:Settings.keys[sender.tag])
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    @IBAction func AdditionalTimeValueChanged(sender: UIStepper) {
+        additionalTimeLabel.text = "\(sender.value)"
+        println(additionalTimeLabel.text)
+        
+        NSUserDefaults.standardUserDefaults().setValue(sender.value, forKey:Settings.keys[sender.tag])
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
     
     @IBAction func tutorialSwitchValueDidChanged(sender: UISwitch) {
         sender.resignFirstResponder()
@@ -83,7 +115,6 @@ class SettingsTableViewController: UITableViewController, UITextFieldDelegate {
         NSUserDefaults.standardUserDefaults().synchronize()
     }
 
-    
     func textFieldDidEndEditing(textField: UITextField) {
         textField.resignFirstResponder();
         NSUserDefaults.standardUserDefaults().setValue(textField.text, forKey:Settings.keys[textField.tag])
