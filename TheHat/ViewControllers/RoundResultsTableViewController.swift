9737e0fde2e04cd9097fcf258021c87728a4f280
Vadim Drobinin <vadim@drobinin.com>
2014-11-24 22:29:14 +0300

    Refactored RoundResultsTable.

--- TheHat/ViewControllers/RoundResultsTableViewController.swift
+++ TheHat/ViewControllers/RoundResultsTableViewController.swift
@@ -8,29 +8,51 @@
 
 import UIKit
 
+/// Class for round results
 class RoundResultsTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate, MGSwipeTableCellDelegate {
 
+    /// Exit to the final results
     @IBOutlet weak var finishGameButton: UIBarButtonItem!
     
+    /// Local Settings object
     var lSettings: LocalSettings?
+    
+    /// Tournament System object
     var tSystem: TournamentSystem?
+    
+    /// Last played word
     var currentWord: Word?
+    
+    /// List of all words from the round
     var cells = [ActiveWord]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(true, animated: true)
         
+        self.navigationItem.setHidesBackButton(true, animated: true)
         self.cells = tSystem!.currentActiveWords
-
     }
     
     // MARK: - New cell with word
     
+    /**
+    Returns number of rows.
+    
+    :param: tableView UITableView with round results
+    :param: section Int section
+    :returns: Int number of rows
+    */
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.cells.count
     }
     
+    /**
+    Creates new cell with a word.
+    
+    :param: tableView UITableView with round results
+    :param: indexPath NSIndexPath for the new cell
+    :returns: CustomTableViewCell object with a word
+    */
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: CustomTableViewCell = tableView.dequeueReusableCellWithIdentifier("CustomCell") as CustomTableViewCell
         let word = self.cells[indexPath.row]
@@ -62,7 +84,16 @@ class RoundResultsTableViewController: UITableViewController, UITableViewDataSou
     // MARK: - Swipe for cell
     
     // TODO: Update word in activeWords array!
+    /**
+    Processes swipe of a cell with a word.
     
+    :param: cell MGSwipeTableCell object which was swiped
+    :param: index Int index of the cell
+    :param: direction MGSwipeDirection object with a direction of the swipe
+    :param: fromExpansion Bool flag process the expansion swipe
+    
+    :returns: Bool true if the cell closes after tap else false
+    */
     func swipeTableCell(cell: MGSwipeTableCell!, tappedButtonAtIndex index: Int, direction: MGSwipeDirection, fromExpansion: Bool) -> Bool {
         switch (index) {
         case 0:
@@ -85,6 +116,11 @@ class RoundResultsTableViewController: UITableViewController, UITableViewDataSou
     
     // MARK: - Segue
     
+    /**
+    Performs a segue with a new round or final results.
+    
+    :param: sender UIBarButtonItem object
+    */
     @IBAction func nextRoundButtonAction(sender: UIBarButtonItem) {
         if (tSystem!.wordsLeft() < 1) {
             performSegueWithIdentifier("showGameResults", sender: nil)
@@ -92,6 +128,13 @@ class RoundResultsTableViewController: UITableViewController, UITableViewDataSou
             performSegueWithIdentifier("nextRoundSegue", sender: nil)
         }
     }
+    
+    /**
+    Prepares for a segue and passes all vital objects to the next screen.
+    
+    :param: segue UIStoryboardSegue
+    :param: sender AnyObject!
+    */
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "showGameResults") {
             var gameResultsVC = segue.destinationViewController as GameResultsViewController;
