d9bd62b3e0215f2af1d907473093d23a56dadff9
Vadim Drobinin <vadim@drobinin.com>
2014-11-26 16:58:42 +0300

    Refactored constants.

--- TheHat/Supporting Files/Constants.swift
+++ TheHat/Supporting Files/Constants.swift
@@ -8,17 +8,22 @@
 
 import Foundation
 
+/// List of global constants
 struct Constants {
     
+    // MARK: - Background colors
     static let lightGreen = UIColor(red: 175.0/255.0, green: 240.0/255.0, blue: 203.0/255.0, alpha: 1.0)
     static let lightGrey = UIColor(red: 255.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
-    
+
+    // MARK: - Game params
     static let playersInRound = 2
     
+    // MARK: - Words settings
     static let deleteColor = UIColor(red: 1.0, green: 0.231, blue: 0.188, alpha: 0.7)
     static let editColor = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 0.7)
     static let addColor = UIColor(red: 0.07, green: 0.75, blue: 0.16, alpha: 0.7)
     
+    // MARK: - Words statuses
     static let OKColor = UIColor(red: 0.07, green: 0.75, blue: 0.16, alpha: 0.7)
     static let FColor = UIColor(red: 1.0, green: 0.231, blue: 0.188, alpha: 0.7)
     static let MColor = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 0.7)
